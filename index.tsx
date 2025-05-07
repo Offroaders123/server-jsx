7c07f380b1d5bb558ba182b71510d5aa00032caf
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-05-06 20:38:53 -0700

    A Client Component
    Need to find a way to distinguish the difference between client and server components. Maybe it can be a builder thing. I want it to type-error at the TS level if you try to call one from another depending on the context you are in.

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import { createSignal } from "solid-js";
 import Date from "./demo-jsx/component.js";
 
 // @DOCTYPE html
@@ -6,6 +7,16 @@ import Date from "./demo-jsx/component.js";
 
 {"My first PHP script!"}
 <Date/>
+<MyButton/>
 
 </body>
 </html>
+
+// This is a client component. How can we distinguish the difference here?
+function MyButton() {
+  const [title, setTitle] = createSignal<string>("Update my name!");
+
+  return (
+    <button onclick={() => setTitle("The New Black")}>{title()}</button>
+  );
+}
