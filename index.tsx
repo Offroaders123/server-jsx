d6ad51988bb19746fdfd5ec15e2cd8523b7c6005
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-05-06 20:47:41 -0700

    A Server Component
    Maybe I can just make this solely server-only for the time being? That might be counter-intuitive to what this could be though. I might have to focus on only that first though, as that will be much more 'straightforward' to set up initially.

--- index.tsx
+++ index.tsx
@@ -8,6 +8,7 @@ import Date from "./demo-jsx/component.js";
 {"My first PHP script!"}
 <Date/>
 <MyButton/>
+<BigList/>
 
 </body>
 </html>
@@ -18,5 +19,16 @@ function MyButton() {
 
   return (
     <button onclick={() => setTitle("The New Black")}>{title()}</button>
+  );
+}
+
+// This is server-side rendered. I don't want this to run on the client.
+// Right now this looks no different than the client one.
+// That's what we have to figure out.
+function BigList() {
+  return (
+    Array.from({ length: 40 }, () =>
+      <em><code>{Math.random()}</code></em>
+    )
   );
 }
