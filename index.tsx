cb40162cfff3780381dba90211ef67d0985777c3
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-05-10 12:46:34 -0700

    App Formatting

--- index.tsx
+++ index.tsx
@@ -2,14 +2,14 @@ import { BigList, Date } from "./demo-jsx/component.js";
 
 export default function App() {
   return (
-<html>
-<body>
+    <html>
+    <body>
 
-{"My first PHP script!"}
-<Date/>
-<BigList/>
+    {"My first PHP script!"}
+    <Date/>
+    <BigList/>
 
-</body>
-</html>
+    </body>
+    </html>
   );
 }
